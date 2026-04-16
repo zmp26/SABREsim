d7255a674c7b11abc52caf1ea433479b932ef79b
Zach Purcell <zpurce2@lsu.edu>
2026-04-16 09:18:21 -0500

    changed resolution, but back to sigma=50keV, thresh=100keV

--- src/SABREsim.cpp
+++ src/SABREsim.cpp
@@ -181,7 +181,7 @@ void SABREsim::InitializeDetectors(bool WriteCornersToFile){
 bool SABREsim::InitializeModels(){
 	//energy resolution models (eventually, config_ should have the ability to set the resolution and cut off below dynamically)
 	for(size_t i=0; i<SABRE_Array_->size(); i++){
-		SABRE_EnergyResolutionModel* m = new SABRE_EnergyResolutionModel(0.05, 0.001);
+		SABRE_EnergyResolutionModel* m = new SABRE_EnergyResolutionModel(0.05, 0.100);
 		SABREARRAY_EnergyResolutionModels_.push_back(m);
 	}
 
