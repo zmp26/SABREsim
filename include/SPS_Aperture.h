db91aa75e05b1027f6000dee0bcdb3764b181567
Zach Purcell <zpurce2@lsu.edu>
2026-04-20 12:23:49 -0500

    SPS_Aperture first commit

---
+++ include/SPS_Aperture.h
@@ -0,0 +1,38 @@
+#ifndef SPS_APERTURE_H
+#define SPS_APERTURE_H
+
+#include <iostream>
+#include <cmath>
+#include <random>
+#include "Vec3.h"
+
+class SPS_Aperture{
+public:
+	SPS_Aperture(double r, 
+				 double thetaMin, double thetaMax,
+				 double phiMin, double phiMax,
+				 double sigE=0., double sigTheta=0., double sigPhi=0.);
+
+	~SPS_Aperture();
+
+	bool IsDetected(const Vec3& traj, const Vec3& origin) const;
+
+	double GetSmearedEnergy(double energy) const;
+	double GetSmearedTheta(double theta) const;
+	double GetSmearedPhi(double phi) const;
+
+	void SetEnergyResolution(double sigma) { sigmaE = sigma; }
+	void SetThetaResolution(double sigma) { sigmaTheta = sigma; }
+	void SetPhiResolution(double sigma) { sigmaPhi = sigma; }
+
+private:
+	double ApplyGaussian(double value, double sigma) const;
+
+	double radius, thetaMin, thetaMax, phiMin, phiMax;
+
+	double sigmaE, sigmaTheta, sigmaPhi;
+
+	mutable std::mt19937 gen;
+};
+
+#endif//SPS_APERTURE_H
---
+++ src/SPS_Aperture.cpp
@@ -0,0 +1,87 @@
+#include "SPS_Aperture.h"
+
+SPS_Aperture::SPS_Aperture(double r,
+						   double thetaMin, double thetaMax,
+						   double phiMin, double phiMax,
+						   double sigmaE, double sigmaTheta, double sigmaPhi)
+	: radius(r), thetaMin(thetaMin), thetaMax(thetaMax), phiMin(phiMin), phiMax(phiMax),
+	  sigmaE(sigmaE), sigmaTheta(sigmaTheta), sigmaPhi(sigmaPhi)
+{
+	std::random_device rd;
+	gen.seed(rd());
+}
+
+SPS_Aperture::~SPS_Aperture() {}
+
+/*
+ *	IsDetected() solves for intersection of a ray with trajectory traj from some
+ *  point origin with a sphere within some bounds [thetaMin, thetaMax] and 
+ *  [phiMin, phiMax] (these are set in the class)
+ *
+ *	Returns true if intersection occurs, false otherwise
+ *	
+ *  Parameterizes as follows:
+ *		Ray: 		P(t) = origin + t*unit_traj
+ *		Sphere: 	|P(t)|^2 = radius^2
+ *
+ *	Solves for the t(s) in the ray that intersects a sphere centered at origin
+ *  and determins if within bounds
+ *
+ *  Quadratic: 
+ *		(O + td) * (O + td) = R^2 where O is the reaction origin and d the trajectory,
+ *		and R the distance from target plane to aperture (* is dot product here)
+ *		==>		(d^2)t^2 + (2(O*d))t + (O^2 - R^2) = 0
+ *		==>		A = d^2 	B = 2(O*d)		C = (O^2 - R^2)
+ *		
+ *	Solved by:
+ *		t = (-2(O*d) +/- SQRT[ 4(O*d) - 4d^2(O^2 - R^2) ]) / (2d^2)
+ *		NOTE: Code chooses positive root
+ */
+bool SPS_Aperture::IsDetected(const  Vec3& traj, const Vec3& origin) const {
+
+	Vec3 d = traj.Unit();
+
+	double A = d.Mag2(d); // = 1 because d is unit vector
+	double B = 2. * origin.Dot(d);
+	double C = origin.Mag2() - radius*radius;
+
+	double disc = (B*B) - 4.*A*C;
+
+	//if disc < 0, trajectory misses sphere entirely... this is unlikely and should never happen, but handle anyway
+	if(disc < 0) return false;
+
+	double t = (-B + std::sqrt(disc)) / (2.*A);
+	if(t<0) return false;
+
+	Vec3 intersectPoint = origin + (d*t);
+
+	double theta = intersectPoint.GetTheta();
+	double phi = intersectPoint.GetPhi();
+	if(phi < 0.) phi += 360.;
+
+	if(theta >= thetaMin && theta <= thetaMax && phi >= phiMin && phi <= phiMax){
+		return true;
+	}
+
+	return false;
+}
+
+double SPS_Aperture::GetSmearedEnergy(double energy) const {
+	return ApplyGaussian(energy, sigmaE);
+}
+
+double SPS_Aperture::GetSmearedTheta(double theta) const {
+	return ApplyGaussian(theta, sigmaTheta);
+}
+
+double SPS_Aperture::GetSmearedPhi(double phi) const {
+	return ApplyGaussian(phi, sigmaPhi);
+}
+
+double SPS_Aperture::ApplyGaussian(souble value, double sigma) const {
+	if(sigma <= 0.) return value;
+
+	std::normal_distribution<double> dist(value,sigma);
+
+	return dist(gen);
+}
