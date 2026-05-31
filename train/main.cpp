b73ae75544a8b0321da2662cd97f701e5823c1ec
Zach Purcell <zmpurcell97@gmail.com>
2026-05-31 13:28:40 -0500

    basic inference template

---
+++ train/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include "include/onnxruntime_cxx_api.h"
+
+void evaluate_bdt() {
+
+	Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "SABRE_BDT_Inference");
+	Ort::SessionOptions session_options;
+	session_options.SetIntraOpNumThreads(1);
+
+	const char* model_path = "sabre_bdt.onnx";
+	std::cout << "Loading model: " << model_path << std::endl;
+	Ort::Session session(env, model_path, session_options);
+
+	std::vector<float> input_tensor_values = {2.35f, 12.4f, -0.05f, 0.12f};
+	std::vector<int64_t> input_node_dims = {1, 4};
+
+	//auto memory_info = Ort::MemoryInfo::CreateCpu(Ort::ArenaAllocator, Ort::MemTypeDefault);
+	auto memory_info = Ort::MemoryInfo::CreateCpu(OrtAllocatorType::OrtArenaAllocator, OrtMemType::OrtMemTypeDefault);
+
+	Ort::Value input_tensor = Ort::Value::CreateTensor<float>(
+		memory_info,
+		input_tensor_values.data(),
+		input_tensor_values.size(),
+		input_node_dims.data(),
+		input_node_dims.size()
+	);
+
+	const char* input_names[] = {"float_input"};
+	const char* output_names[] = {"label", "probabilities"};
+
+	auto output_tensors = session.Run(
+		Ort::RunOptions{nullptr},
+		input_names,
+		&input_tensor, 1,
+		output_names, 2
+	);
+
+
+	float* probabilities = output_tensors[1].GetTensorMutableData<float>();
+
+	float prob_true_permutation = probabilities[1];
+
+	std::cout << "--- BDT Evaluation ---" << std::endl;
+	std::cout << "Probability of true permutation: " << prob_true_permutation * 100. << "%" << std::endl;
+}
+
+int main() {
+	evaluate_bdt();
+	return 0;
+}
